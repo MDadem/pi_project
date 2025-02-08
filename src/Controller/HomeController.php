1d0962612e15d56901c7724592f6e156f90653f1
Adem Miladi <91350744+MDadem@users.noreply.github.com>
2025-02-08 19:50:40 +0100

    back-office setup

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,10 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController{
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/base.html.twig', [
+        return $this->render('frontend/home/base.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
@@ -18,10 +18,52 @@ final class HomeController extends AbstractController{
     #[Route('/auth', name: 'app_auth')]
     public function auth(): Response
     {
-        return $this->render('auth/auth.html.twig', [
+        return $this->render('frontend/auth/auth.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('backend/home/home.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+    #[Route('/dashboard/sign_in', name: 'app_dashboard_sign_in')]
+    public function dashboard_sign_in(): Response
+    {
+        return $this->render('backend/signin/signin.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/dashboard/sign_up', name: 'app_dashboard_sign_up')]
+    public function dashboard_sign_up(): Response
+    {
+        return $this->render('backend/signup/signup.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/dashboard/404', name: 'app_dashboard_404')]
+    public function dashboard_404(): Response
+    {
+        return $this->render('backend/404/404error.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+    #[Route('/dashboard/blank', name: 'app_dashboard_blank')]
+    public function dashboard_blank(): Response
+    {
+        return $this->render('backend/blankpage/blank.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
 
 
 }
